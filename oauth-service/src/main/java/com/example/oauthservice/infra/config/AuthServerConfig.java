86b335aa52eeaf60fd901538e87667d120c5c39d
yongBomb <yongbomb90@gmail.com>
2022-03-27 23:30:06 +0900

    oauth default setting type 1

--- oauth-service/src/main/java/com/example/oauthservice/infra/config/AuthServerConfig.java
+++ oauth-service/src/main/java/com/example/oauthservice/infra/config/AuthServerConfig.java
@@ -1,5 +1,10 @@
 package com.example.oauthservice.infra.config;
 
+import com.example.oauthservice.module.account.domain.AccountClient;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +14,25 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.UUID;
 
 @Configuration(proxyBeanMethods = false)
 public class AuthServerConfig {
@@ -25,14 +47,69 @@ public class AuthServerConfig {
     @Bean
     RegisteredClientRepository registeredClientRepository () {
         // TODO : client 구성
-        return null;
+        return new RegisteredClientRepository() {
+            @Override
+            public void save(RegisteredClient registeredClient) {
+
+            }
+
+            @Override
+            public RegisteredClient findById(String id) {
+                return new AccountClient();
+            }
+
+            @Override
+            public RegisteredClient findByClientId(String clientId) {
+                return new AccountClient();
+            }
+        };
+    }
+
+    private static KeyPair generateRsaKey() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    private static RSAKey generateRsa() throws NoSuchAlgorithmException {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+        // @formatter:off
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        // @formatter:on
     }
 
     @Bean
-    JWKSource<SecurityContext> jwkSource(){
-        // TODO : JWS 구성
-        return null;
+    public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
+        RSAKey rsaKey = generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
+
+    @Bean
+    ProviderSettings providerSettings() {
+        return new ProviderSettings().issuer("http://localhost:8888");
+    }
+
+    @Bean
+    public TokenSettings tokenSettings() {
+        //@formatter:off
+        return new TokenSettings().accessTokenTimeToLive(Duration.ofDays(1));
+        // @formatter:on
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
 
 
 }
---
+++ oauth-service/src/main/java/com/example/oauthservice/module/account/domain/AccountClient.java
@@ -0,0 +1,7 @@
+package com.example.oauthservice.module.account.domain;
+
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+
+public class AccountClient extends RegisteredClient {
+
+}
