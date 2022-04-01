b069f866819e2536924f824103ffe1c2539fe2cb
yongBomb <yongbomb90@gmail.com>
2022-04-01 22:02:27 +0900

    auth old version

---
+++ oauth-service-old/src/main/java/com/example/oauthservice/OauthServiceOldApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServiceOldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthServiceOldApplication.class, args);
+    }
+
+}
---
+++ oauth-service-old/src/main/java/com/example/oauthservice/infra/conf/AuthrizationConfig.java
@@ -0,0 +1,13 @@
+package com.example.oauthservice.infra.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@EnableAuthorizationServer
+@Configuration
+public class AuthrizationConfig {
+
+
+
+
+}
