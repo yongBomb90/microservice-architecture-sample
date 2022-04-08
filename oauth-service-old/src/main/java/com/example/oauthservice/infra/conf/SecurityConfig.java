6a6659ace28c79d42b6682cabdaa69f3c20e2f39
yongBomb <yongbomb90@gmail.com>
2022-04-08 22:24:39 +0900

    properties set

--- oauth-service-old/src/main/java/com/example/oauthservice/infra/conf/SecurityConfig.java
+++ oauth-service-old/src/main/java/com/example/oauthservice/infra/conf/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.example.oauthservice.infra.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -65,10 +66,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         tokenStore.setApprovalStore(approvalStore());
         return tokenStore;
     }
-
-
-
-
 
 
 }
--- oauth-service-old/src/main/java/com/example/oauthservice/module/account/jpa/entity/AccountEntity.java
+++ oauth-service-old/src/main/java/com/example/oauthservice/module/account/jpa/entity/AccountEntity.java
@@ -1,4 +1,12 @@
 package com.example.oauthservice.module.account.jpa.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class AccountEntity {
+
+    @Id
+    long id;
+
 }
