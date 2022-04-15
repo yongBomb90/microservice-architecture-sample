c7fac48a79232db0c5438324fe8c3da0a361dd7d
yongBomb <yongbomb90@gmail.com>
2022-04-15 22:00:09 +0900

    setting

---
+++ oauth-service-old/src/main/java/com/example/oauthservice/module/constant/MessageConstants.java
@@ -0,0 +1,2 @@
+package com.example.oauthservice.module.constant;public class MessageConstant {
+}
---
+++ oauth-service-old/src/main/java/com/example/oauthservice/module/constant/ModuleConstants.java
@@ -0,0 +1,4 @@
+package com.example.oauthservice.module.constant;
+
+public class ModuleConstant {
+}
