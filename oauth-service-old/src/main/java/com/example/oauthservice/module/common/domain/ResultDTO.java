4cd81469ea3cfbcccc8f1b673dda2cc143675422
yongBomb <yongbomb90@gmail.com>
2022-04-23 22:52:25 +0900

    resultVO

--- oauth-service-old/src/main/java/com/example/oauthservice/module/common/domain/ResultDTO.java
+++ oauth-service-old/src/main/java/com/example/oauthservice/module/common/domain/ResultDTO.java
@@ -1,5 +1,29 @@
 package com.example.oauthservice.module.common.domain;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+
 //TODO ResponseEntity override
-public class ResultDTO {
+public class ResultDTO extends ResponseEntity {
+
+    public ResultDTO(HttpStatus status) {
+        super(status);
+    }
+
+    public ResultDTO(Object body, HttpStatus status) {
+        super(body, status);
+    }
+
+    public ResultDTO(MultiValueMap headers, HttpStatus status) {
+        super(headers, status);
+    }
+
+    public ResultDTO(Object body, MultiValueMap headers, HttpStatus status) {
+        super(body, headers, status);
+    }
+
+    public ResultDTO(Object body, MultiValueMap headers, int rawStatus) {
+        super(body, headers, rawStatus);
+    }
 }
