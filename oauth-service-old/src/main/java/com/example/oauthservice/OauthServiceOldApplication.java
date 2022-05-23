a649068101f7559c472c127add53ccc1bb2297d0
yongBomb <yongbomb90@gmail.com>
2022-05-23 22:41:07 +0900

    todo list

--- oauth-service-old/src/main/java/com/example/oauthservice/OauthServiceOldApplication.java
+++ oauth-service-old/src/main/java/com/example/oauthservice/OauthServiceOldApplication.java
@@ -3,6 +3,14 @@ package com.example.oauthservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/******
+ * TODO :
+ * TokenGnerer 를 통한 토큰 생성 규칙 첨가
+ * TokenApprove 를 통항 리플레시 토큰 규칙 추가
+ *
+ *
+ */
+
 @SpringBootApplication
 public class OauthServiceOldApplication {
     //https://knoc-story.tistory.com/80
